7c88563f84981df52db8aae7776e9fb41919c08c
Wood <1831051389@qq.com>
2023-09-01 09:54:44 +0800

    OLED功能开发完毕
    OLED显示包含：转速、转速设定、电机温度

--- Tasks/Src/OLED_task.cpp
+++ Tasks/Src/OLED_task.cpp
@@ -3,6 +3,8 @@
 #include "cmsis_os.h"
 #include "FreeRTOS.h"
 
+extern revolver_task_t revolver;
+
 void OLED_task(void const *pvParameters)
 {
     OLED_init();
@@ -10,7 +12,20 @@ void OLED_task(void const *pvParameters)
 
     while (1)
     {
-        OLED_show_string(0, 0, "Hello World!");
+        OLED_operate_gram(PEN_CLEAR);
+
+        OLED_printf(1, 8, "%.0f", revolver.fric_speed_offset + BASE_SPEED);
+
+        OLED_printf(0, 16, "%d", revolver.fric_motor[0].motor_measure->speed_rpm);
+        OLED_printf(0, 0, "%d", revolver.fric_motor[1].motor_measure->speed_rpm);
+        OLED_printf(4, 16, "%d", revolver.fric_motor[2].motor_measure->speed_rpm);
+        OLED_printf(4, 0, "%d", revolver.fric_motor[3].motor_measure->speed_rpm);
+
+        OLED_printf(2, 14, "%d", revolver.fric_motor[0].motor_measure->temperate);
+        OLED_printf(2, 5, "%d", revolver.fric_motor[1].motor_measure->temperate);
+        OLED_printf(3, 14, "%d", revolver.fric_motor[2].motor_measure->temperate);
+        OLED_printf(3, 5, "%d", revolver.fric_motor[3].motor_measure->temperate);
+
         OLED_refresh_gram();
         vTaskDelay(2);
     }
