48fc7ea9bd35aba6d4619f88c7ad188ba5467c5d
Wood <1831051389@qq.com>
2023-09-08 19:34:59 +0800

    refactor：led闪烁任务改用累加计数
    led闪烁的任务改用累加计数来实现并做了封装。monitor任务转换为UTF-8编码。测试通过

--- Tasks/Src/monitor_task.cpp
+++ Tasks/Src/monitor_task.cpp
@@ -1,52 +1,94 @@
 #include "monitor_task.h"
 #include "cmsis_os.h"
 #include "main.h"
+#include "task.h"
 
 #define RGB_FLOW_COLOR_CHANGE_TIME  1000
 #define RGB_FLOW_COLOR_LENGHT   6
 //blue-> green(dark)-> red -> blue(dark) -> green(dark) -> red(dark) -> blue
-//À¶ -> ÂÌ(Ãð) -> ºì -> À¶(Ãð) -> ÂÌ -> ºì(Ãð) -> À¶ 
+//è“ -> ç»¿(ç­) -> çº¢ -> è“(ç­) -> ç»¿ -> çº¢(ç­) -> è“ 
 uint32_t RGB_flow_color[RGB_FLOW_COLOR_LENGHT + 1] = {0xFF0000FF, 0x0000FF00, 0xFFFF0000, 0x000000FF, 0xFF00FF00, 0x00FF0000, 0xFF0000FF};
 extern CAN_HandleTypeDef hcan1;
 extern CAN_HandleTypeDef hcan2;
+
+static void led_monitor(void);
+
 /**
   * @brief          led rgb task
   * @param[in]      pvParameters: NULL
   * @retval         none
   */
 /**
-  * @brief          led RGBÈÎÎñ
+  * @brief          led RGBä»»åŠ¡
   * @param[in]      pvParameters: NULL
   * @retval         none
   */
-void monitor_task(void const * argument)
+void monitor_task(void const *argument)
 {
-    while(1)
-    {
-			if(vision_info_point()->RxPacketSed.SOF==0xB5)
-			{
-				//ÉÁÀ¶µÆ ÊÓ¾õÍ¨ÐÅÕý³£
-				aRGB_led_show(0xFF0000FF);
-				osDelay(200);
-				aRGB_led_show(0x000000FF);
-				osDelay(200);
-			} else if((toe_is_error(CAN_TOE) == 1))
-			{
-				//ÉÁºìµÆ ¿ìÉÁ CANÏßÉè±¸µôÏß
-				aRGB_led_show(0xFFFF0000);
-				osDelay(100);
-				aRGB_led_show(0x00FF0000);
-				osDelay(100);
-			} else
-			{
-				//ÉÁÂÌµÆ	  Õý³£ÔËÐÐ ÎÞÊÓ¾õ
-				aRGB_led_show(0xFF00FF00);
-				osDelay(200);
-				aRGB_led_show(0x0000FF00);
-				osDelay(200);
-			}
-			  
-    }
+	while (1)
+	{
+		led_monitor();
+		vTaskDelay(2);
+	}
 }
 
+static void led_monitor(void)
+{
+	static uint16_t tick = 0;
 
+	//é—ªè“ç¯ï¼Œè§†è§‰é€šä¿¡æ­£å¸¸
+	if (vision_info_point()->RxPacketSed.SOF == 0xB5)
+	{
+		if (tick < 100)
+		{
+			aRGB_led_show(0xFF0000FF);
+			tick++;
+		}
+		else if (tick < 200)
+		{
+			aRGB_led_show(0x000000FF);
+			tick++;
+		}
+		else
+		{
+			tick = 0;
+		}
+	}
+	//çº¢ç¯å¿«é—ªï¼ŒCANçº¿è®¾å¤‡æŽ‰çº¿
+	else if ((toe_is_error(CAN_TOE) == 1))
+	{
+		if (tick < 50)
+		{
+			aRGB_led_show(0xFFFF0000);
+			tick++;
+		}
+
+		else if (tick < 100)
+		{
+			aRGB_led_show(0x00FF0000);
+			tick++;
+		}
+		else
+		{
+			tick = 0;
+		}
+	}
+	//é—ªç»¿ç¯ï¼Œæ­£å¸¸è¿è¡Œï¼Œæ— è§†è§‰
+	else
+	{
+		if (tick < 100)
+		{
+			aRGB_led_show(0xFF00FF00);
+			tick++;
+		}
+		else if (tick < 200)
+		{
+			aRGB_led_show(0x0000FF00);
+			tick++;
+		}
+		else
+		{
+			tick = 0;
+		}
+	}
+}
