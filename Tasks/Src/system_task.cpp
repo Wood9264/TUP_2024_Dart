1b8350369d320203f1cd172d7c9b8c2955e8dbe7
Wood <1831051389@qq.com>
2024-02-24 14:59:10 +0800

    fix: 修复中断发射流程后电机还会异常转动的bug

--- Tasks/Src/system_task.cpp
+++ Tasks/Src/system_task.cpp
@@ -143,6 +143,7 @@ void system_t::mode_transit()
         load_point()->rotary_motor.has_shoot_init_started = 0;
         load_point()->rotary_motor.has_shoot_init_finished = 0;
 
+        has_index_added = 0;
         strike_target = OUTPOST;
     }
 
@@ -159,14 +160,21 @@ void system_t::mode_transit()
     {
         revolver_point()->fric_wheel_group.is_fric_wheel_on = 0;
 
+        revolver_point()->yaw_motor.ecd_set = revolver_point()->yaw_motor.accumulate_ecd;
         revolver_point()->yaw_motor.has_shoot_init_started = 0;
         revolver_point()->yaw_motor.has_shoot_init_finished = 0;
 
+        load_point()->loader_motor.ecd_set = load_point()->loader_motor.accumulate_ecd;
         load_point()->loader_motor.has_shoot_init_started = 0;
         load_point()->loader_motor.has_shoot_init_finished = 0;
 
+        load_point()->rotary_motor.final_relative_angle_set = load_point()->rotary_motor.relative_angle;
+        load_point()->rotary_motor.relative_angle_set = load_point()->rotary_motor.relative_angle;
         load_point()->rotary_motor.has_shoot_init_started = 0;
         load_point()->rotary_motor.has_shoot_init_finished = 0;
+
+        has_index_added = 0;
+        strike_target = OUTPOST;
     }
 }
 
