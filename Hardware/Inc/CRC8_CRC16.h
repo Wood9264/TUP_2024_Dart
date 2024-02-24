f7a442e57334cb0c6382bbb1587b67396e6e80a1
Wood <1831051389@qq.com>
2024-02-24 21:33:40 +0800

    feat: 新增CRC16_MODBUS校验

--- Hardware/Inc/CRC8_CRC16.h
+++ Hardware/Inc/CRC8_CRC16.h
@@ -112,6 +112,23 @@ extern uint32_t verify_CRC16_check_sum(uint8_t *pchMessage, uint32_t dwLength);
   */
 extern void append_CRC16_check_sum(uint8_t * pchMessage,uint32_t dwLength);
 
+/**
+ * @brief          计算CRC16 MODBUS
+ * @param[in]      pch_message: 数据
+ * @param[in]      dw_length: 数据长度
+ * @param[in]      wCRC:初始CRC16
+ * @retval         计算完的CRC16
+ */
+extern uint16_t get_CRC16_MODBUS_check_sum(uint8_t *pch_message, uint32_t dw_length, uint16_t wCRC);
+
+/**
+ * @brief          CRC16 MODBUS校验函数
+ * @param[in]      pch_message: 数据
+ * @param[in]      dw_length: 数据和校验的长度
+ * @retval         真或者假
+ */
+extern uint32_t verify_CRC16_MODBUS_check_sum(uint8_t *pchMessage, uint32_t dwLength);
+
 #endif
 
 #ifdef __cplusplus
