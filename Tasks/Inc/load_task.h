9b06ed07b70b715cbfce2f76fa468559388e945e
Wood <1831051389@qq.com>
2024-04-14 10:47:29 +0800

    param: 调节行程参数、转盘零点和转盘容忍误差

--- Tasks/Inc/load_task.h
+++ Tasks/Inc/load_task.h
@@ -51,11 +51,11 @@
 #define ROTARY_SHOOT_RAMP_BUFF 0.003f
 
 //转盘电机零点编码值
-#define ROTARY_ZERO_POINT_ECD 6692
+#define ROTARY_ZERO_POINT_ECD 2633
 //装填机构处在载弹架中时转盘电机可转动角度的一半，单位为弧度
 #define ROTARY_HALF_MOVABLE_ANGLE 0.166f
 //装填电机最大前进距离的编码值
-#define LOADER_FORWARD_ECD 562312
+#define LOADER_FORWARD_ECD 499253
 //装填电机受限时最大前进距离的编码值
 #define LOADER_RESTRICT_FORWARD_ECD 16385
 //校准时零点编码值的补偿量。防止装填电机回退时因为超调碰到触点开关
@@ -73,7 +73,7 @@
 #define LOADER_SHOOT_DOWN_SPEED 8
 
 //转盘电机运动到指定位置时允许的角度误差，单位为弧度
-#define ROTARY_ANGLE_TOLERANCE 0.01f
+#define ROTARY_ANGLE_TOLERANCE 0.0038f
 //装填电机运动到指定位置时允许的编码值误差
 #define LOADER_ECD_TOLERANCE 8192
 
